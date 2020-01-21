e1de8a4b8a0b05b796563e27e43b3968d4abc8f7
Radames Ajna <radamajna@gmail.com>
2020-01-21 14:03:18 -0800

    mixed features images on text body

--- modules/scripts/dataFetcher.js
+++ modules/scripts/dataFetcher.js
@@ -81,7 +81,7 @@ function markdown2html (data) {
         }
         if (key === 'text') {
           configDom = {
-            ALLOWED_TAGS: ['a', 'p'],
+            ALLOWED_TAGS: ['a', 'p', 'img'],
             KEEP_CONTENT: true
           }
         }
