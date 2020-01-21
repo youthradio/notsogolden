932ab746e5d9d05c1a09a27fc4254e9a815bcc2d
Radames Ajna <radamajna@gmail.com>
2020-01-21 14:20:31 -0800

    allow divs on text for captions

--- modules/scripts/dataFetcher.js
+++ modules/scripts/dataFetcher.js
@@ -81,7 +81,7 @@ function markdown2html (data) {
         }
         if (key === 'text') {
           configDom = {
-            ALLOWED_TAGS: ['a', 'p', 'img'],
+            ALLOWED_TAGS: ['a', 'p', 'img', 'div'],
             KEEP_CONTENT: true
           }
         }
