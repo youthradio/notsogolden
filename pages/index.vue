c739ab271ae63fb9cbcdb1a81edb786a001f0f0e
Radames Ajna <radamajna@gmail.com>
2020-01-27 12:26:05 -0800

    credits sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container">
     <HeaderContainer :article-data="articleData" />
-    <article class="b-copy" v-html="articleData.text " />
+    <article
+      class="b-copy"
+      v-html="articleData.text "
+    />
     <article>
       <SlideContainer
         v-for="theme in articleData.themes"
@@ -10,6 +13,23 @@
         class="slide-container"
         @onTopicChange="onTopicChange"
       />
+      <div class="credits">
+        <h2>Credits</h2>
+        <div
+          v-for="credit in articleData.credits"
+          :key="credit"
+        >
+          <h4>{{ credit.title }}</h4>
+          <ul>
+            <li
+              v-for="names in credit.list"
+              :key="names"
+            >
+              {{ names }}
+            </li>
+          </ul>
+        </div>
+      </div>
     </article>
   </div>
 </template>
@@ -58,7 +78,7 @@ export default {
   padding: 0 0 0 0.2rem;
   width: 100%;
 }
-.title{
+.title {
   font-family: $baseFont;
   font-size: 3rem;
 }
@@ -74,5 +94,18 @@ export default {
   opacity: 0.5;
   transform: translateX(+100%);
 }
-
+.credits {
+  margin-top: 2rem;
+  font-size: 0.8rem;
+  h4 {
+    font-weight: bold;
+    font-size: 0.8rem;
+  }
+  ul {
+    margin-top: 0;
+    margin-bottom: 0rem;
+    padding-left: 0.5rem;
+    list-style: none;
+  }
+}
 </style>
