cf87e6bb70a75f6dd30fb1b0cd2a6ebccb454f3b
Radames Ajna <radamajna@gmail.com>
2020-01-27 18:11:35 -0800

    SEO images

--- post.config.js
+++ post.config.js
@@ -1,14 +1,14 @@
 export default {
   baseURL: '',
   title: 'Not So Golden: Struggles Facing California’s Young Adults',
-  author: 'Static Author',
+  author: '',
   publishDate: 'Oct. 2, 2019',
   location: 'Oakland, CA',
-  description: 'SEO friendly Description',
-  url: 'CANONICAL URL',
-  featureImage: 'https://interactive.yr.media/college-app-guide/images/college-guide-featurex1875.jpg',
-  featureImagePath: 'images/template-feature-image',
-  featureImageDescription: 'Woman looking at kiosk.',
+  description: 'For many young adults, living in the country’s most expensive state is not easy. “Not So Golden” offers a glimpse into how many California young adults are making it work. This is a collaboration project by the California Youth Media Network.',
+  url: 'https://notsogolden.org/',
+  featureImage: 'social/NotSoGolden-feature_social-thumbnail.jpg',
+  featureImagePath: 'social/NotSoGolden-feature_social-thumbnail.jpg',
+  featureImageDescription: 'Golden Gate Illustration ',
   featureImageCaption: '(Photo: Andersen Ross Photography Inc/Getty Images)',
   // you might not need to change
   twitterHandler: '@youthmediaBHC'
